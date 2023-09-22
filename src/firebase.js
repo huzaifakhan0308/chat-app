75f71ab05739b4a730ee77077a98dce2d772e47b
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-09-22 15:12:26 +0500

    set user registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDCl6EWAFXxXoCjIRiDfnRPZUL7BA2h5kg",
     authDomain: "chat-hub-d65dd.firebaseapp.com",
@@ -16,6 +13,7 @@ const firebaseConfig = {
     measurementId: "G-CGSYQEWJ44"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore()
