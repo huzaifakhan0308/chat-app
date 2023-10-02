fa884c64b35eb5f789796f63ba01c804fbd06e7f
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-10-02 15:52:09 +0500

    finish project ready to deployment

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-function Message() {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
     <div
-      className='message'
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
     >
       <div className="messageInfo">
         <img
-          src=''
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
           alt=""
         />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>hjkhjk</p>
-        <img src="" alt="" />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import Add from "../img/addAvatar.png"
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
 
-function Register() {
+const Register = () => {
     const [err, setErr] = useState(false);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
@@ -58,30 +58,31 @@ function Register() {
             setLoading(false);
         }
     };
-  return (
-      <div className="formContainer">
-          <div className="formWrapper">
-              <span className="logo">Lama Chat</span>
-              <span className="title">Register</span>
-              <form onSubmit={handleSubmit}>
-                  <input required type="text" placeholder="display name" />
-                  <input required type="email" placeholder="email" />
-                  <input required type="password" placeholder="password" />
-                  <input required style={{ display: "none" }} type="file" id="file" />
-                  <label htmlFor="file">
-                      <img src={Add} alt="" />
-                      <span>Add an avatar</span>
-                  </label>
-                  <button disabled={loading}>Sign up</button>
-                  {loading && "Uploading and compressing the image please wait..."}
-                  {err && <span>Something went wrong</span>}
-              </form>
-              <p>
-                  You do have an account? <Link to="/register">Login</Link>
-              </p>
-          </div>
-      </div>
-  )
-}
 
-export default Register
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Chat Hub</span>
+                <span className="title">Register</span>
+                <form onSubmit={handleSubmit}>
+                    <input required type="text" placeholder="display name" />
+                    <input required type="email" placeholder="email" />
+                    <input required type="password" placeholder="password" />
+                    <input required style={{ display: "none" }} type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button disabled={loading}>Sign up</button>
+                    {loading && "Uploading and compressing the image please wait..."}
+                    {err && <span>Something went wrong</span>}
+                </form>
+                <p>
+                    You do have an account? <Link to="/register">Login</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
