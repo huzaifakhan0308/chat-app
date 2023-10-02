621bf5190d8c7ec3fb5e55954a4aad458327801b
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-10-02 15:41:22 +0500

    add chat functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
-function Chat() {
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>abcd</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
@@ -19,7 +22,7 @@ function Chat() {
       <Messages />
       <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
